3b84d5a74d9605f57110fcf64d3a125fa5046d4c
Sunminder Sandhu <maca>
2019-11-21 23:39:16 -0800

    Registration setup

---
+++ healthGram/healthGram/Controller/HealthLabRegistrationVC.swift
@@ -0,0 +1,30 @@
+//
+//  HealthLabRegistrationVC.swift
+//  healthGram
+//
+//  Created by Sunminder Sandhu on 11/21/19.
+//  Copyright © 2019 Sunminder Sandhu. All rights reserved.
+//
+
+import UIKit
+
+
+class HealthLabRegistrationVC: UIViewController {
+
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func onNewAccount(_ sender: Any) {
+     
+    }
+    
+}
