74ca6fb81b0118fd06ec646636cd6d1860b2da5b
Anmol Gondara <agondara@mail.sfsu.edu>
2019-11-22 21:02:30 -0800

    Created Tab Controller, Exercises & Profile Nav Controllers, and Started on Exercises and Profile Pages

---
+++ healthGram/healthGram/Controller/HealthGramExercisesVC.swift
@@ -0,0 +1,30 @@
+//
+//  HealthGramExercisesVC.swift
+//  healthGram
+//
+//  Created by Anmol Gondara on 11/22/19.
+//  Copyright © 2019 Russell Wong. All rights reserved.
+//
+
+import UIKit
+
+class HealthGramExercisesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ healthGram/healthGram/Controller/HealthLabProfileVC.swift
@@ -0,0 +1,33 @@
+//
+//  HealthLabProfileVC.swift
+//  healthGram
+//
+//  Created by Anmol Gondara on 11/22/19.
+//  Copyright © 2019 Russell Wong. All rights reserved.
+//
+
+import UIKit
+
+class HealthLabProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        //ToDo: Add logic after firebase is set up
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
