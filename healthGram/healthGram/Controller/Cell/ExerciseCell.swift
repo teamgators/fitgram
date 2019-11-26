54b3d3454d5e1897c750be778d8866cc5ca0d98f
Anmol Gondara <agondara@mail.sfsu.edu>
2019-11-25 22:07:32 -0800

    Added Table View and Cell for Exercise Page

---
+++ healthGram/healthGram/Controller/Cell/ExerciseCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExerciseCell.swift
+//  healthGram
+//
+//  Created by Anmol Gondara on 11/25/19.
+//  Copyright © 2019 Russell Wong. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
