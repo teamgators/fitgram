c27dd25beaf333c58bd70227566318217e1981fb
russ3llwong <russellwong97@hotmail.com>
2019-12-12 01:13:29 -0800

    Configured usernameLabel to grab user's firstName from database

--- healthGram/healthGram/Post.swift
+++ healthGram/healthGram/Post.swift
@@ -15,11 +15,13 @@ class Post{
     
     var caption: String!
     var imageDownloadUrl: String?
+    var uid: String!
     
     init(snapshot: DataSnapshot){
         let json = JSON(snapshot.value!)
         self.imageDownloadUrl = json["imageUrl"].stringValue
         self.caption = json["caption"].stringValue
+        self.uid = json["uid"].stringValue
     }
     
 }
